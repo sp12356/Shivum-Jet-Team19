5f40c2b4bf9ff647dc0f4da5bc3b9d928b79992e
Shivum Pandove <shivumpandove@Shivums-MacBook-Pro.local>
2024-07-31 14:41:13 -0400

    Finalized edits

--- chameleon/src/components/Home.js
+++ chameleon/src/components/Home.js
@@ -5,14 +5,6 @@ function HomePage() {
     <div className="App">
       <header className="App-header">
         <h1>Chameleon</h1>
-        <form>
-          <h5>Username</h5>
-          <textarea name="User" cols="30" rows="2"></textarea>
-        </form>
-        <form>
-          <h5>Password</h5>
-          <textarea name="Password" cols="30" rows="2"></textarea>
-        </form>
       </header>
     </div>
   );
---
+++ my_script.py
@@ -0,0 +1,7 @@
+def run_script():
+    # Simulate some computation
+    result = {
+        "message": "Hello from Python!",
+        "data": [1, 2, 3, 4, 5]
+    }
+    return result
